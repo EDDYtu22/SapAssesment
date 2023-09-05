08c5f9ff19d0b04d0f2e0d83600357788a21dc38
Edmond Yordanov <edmond.yordanov@mentormate.university.com>
2023-09-05 23:24:58 +0300

    initial commit

---
+++ src/main/java/dev/edmond/sapassesment/SapAssesmentApplication.java
@@ -0,0 +1,13 @@
+package dev.edmond.sapassesment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SapAssesmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SapAssesmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/edmond/sapassesment/SapAssesmentApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.edmond.sapassesment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SapAssesmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
