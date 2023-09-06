74f2ea4a3c69e569a9a0db1374bce38ec56ba1d5
Edmond Yordanov <edmond.yordanov@mentormate.university.com>
2023-09-06 04:37:29 +0300

    feat: added controller for file manipulatin endpoints

--- src/main/java/dev/edmond/sapassesment/controller/FileController.java
+++ src/main/java/dev/edmond/sapassesment/controller/FileController.java
@@ -1,2 +1,44 @@
-package dev.edmond.sapassesment.controller;public class FileController {
+package dev.edmond.sapassesment.controller;
+
+import dev.edmond.sapassesment.dto.FileCheckDto;
+import dev.edmond.sapassesment.dto.LineSwitchRequest;
+import dev.edmond.sapassesment.dto.WordSwitchRequest;
+import dev.edmond.sapassesment.service.FileService;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.FileNotFoundException;
+
+@RestController
+@RequestMapping("/api/file")
+@RequiredArgsConstructor
+public class FileController {
+
+    private final FileService fileService;
+
+
+    @PostMapping("/check-file")
+    public ResponseEntity<String> checkFile(@RequestBody FileCheckDto fileCheckDto){
+
+        return ResponseEntity.ok().body(fileService.isValidFile(fileCheckDto));
+
+    }
+
+    @PostMapping("/switch-lines")
+    public ResponseEntity<String> switchFileLines(@RequestBody LineSwitchRequest lineSwitchRequest){
+
+        return ResponseEntity.ok().body(fileService.switchLines(lineSwitchRequest));
+
+    }
+
+    @PostMapping("/switch-words")
+    public ResponseEntity<String> switchFileWords(@RequestBody WordSwitchRequest wordSwitchRequest){
+
+        return ResponseEntity.ok().body(fileService.switchWords(wordSwitchRequest));
+
+    }
+
 }
