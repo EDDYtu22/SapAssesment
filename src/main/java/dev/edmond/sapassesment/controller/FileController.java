f30452acb0a81c52a3ef6be30571db2f3fab37a5
Edmond Yordanov <edmond.yordanov@mentormate.university.com>
2023-09-06 12:24:17 +0300

    imports optimisation

--- src/main/java/dev/edmond/sapassesment/controller/FileController.java
+++ src/main/java/dev/edmond/sapassesment/controller/FileController.java
@@ -4,13 +4,12 @@ import dev.edmond.sapassesment.dto.FileCheckDto;
 import dev.edmond.sapassesment.dto.LineSwitchRequest;
 import dev.edmond.sapassesment.dto.WordSwitchRequest;
 import dev.edmond.sapassesment.service.FileService;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
-
-import java.io.FileNotFoundException;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/file")
