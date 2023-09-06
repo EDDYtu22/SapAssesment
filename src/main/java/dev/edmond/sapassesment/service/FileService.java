bbdbd49ca45bc7de0010f4da4f9a710ad21d1534
Edmond Yordanov <edmond.yordanov@mentormate.university.com>
2023-09-06 12:17:37 +0300

    fix: fixed indexing problem

--- src/main/java/dev/edmond/sapassesment/service/FileService.java
+++ src/main/java/dev/edmond/sapassesment/service/FileService.java
@@ -6,8 +6,8 @@ import dev.edmond.sapassesment.dto.WordSwitchRequest;
 import org.springframework.stereotype.Service;
 
 import java.io.*;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @Service
@@ -19,7 +19,7 @@ public class FileService {
         if (file.exists() && file.getName().endsWith(".txt")) {
             return "Valid File!";
         } else {
-            return "Invalid File!";
+            throw new RuntimeException("File doesn't exist or is in invalid format!");
         }
     }
 
@@ -44,21 +44,18 @@ public class FileService {
             throw new RuntimeException(exception.getMessage());
         }
 
-        // Check if the line indices are valid
         if (firstLine < 1 || firstLine > lines.size() || secondLine < 1 || secondLine > lines.size()) {
             throw new IllegalArgumentException("Incorrect line indexes");
         }
 
-        // Switch the lines (assuming 1-based indices)
         String temp = lines.get(firstLine - 1);
         lines.set(firstLine - 1, lines.get(secondLine - 1));
         lines.set(secondLine - 1, temp);
 
-        // Write the modified content back to the file
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(lineSwitchRequest.getFilePath()))) {
             for (String updatedLine : lines) {
                 writer.write(updatedLine);
-                writer.newLine(); // Add a newline after each line
+                writer.newLine();
             }
         } catch (IOException e) {
         }
@@ -89,35 +86,31 @@ public class FileService {
             throw new RuntimeException(exception.getMessage());
         }
 
-        // Check if the line indices are valid
         if (firstLine < 1 || firstLine > lines.size() || secondLine < 1 || secondLine > lines.size()) {
             throw new IllegalArgumentException("Incorrect line indexes");
         }
 
-        List<String> firstLineWords = Arrays.stream(lines.get(firstLine - 1).split("\s+")).toList();
-        List<String> secondLineWords = Arrays.stream(lines.get(secondLine - 1).split("\s+")).toList();
+        String[] firstLineWords = lines.get(firstLine - 1).split("\s+");
+        String[] secondLineWords = lines.get(secondLine - 1).split("\s+");
 
-        if (firstLineWordIndex < 1 || firstLineWordIndex > firstLineWords.size() ||
-                secondLineWordIndex < 1 || secondLineWordIndex > secondLineWords.size()) {
+        if (firstLineWordIndex < 1 || firstLineWordIndex > firstLineWords.length ||
+                secondLineWordIndex < 1 || secondLineWordIndex > secondLineWords.length) {
             throw new IllegalArgumentException("Invalid word indexes");
         }
 
-//        lines.set(firstLineWordIndex - 1, String.join(" ", firstLineWords));
-//        lines.set(secondLineWordIndex - 1, String.join(" ", secondLineWords));
 
-        String temp = firstLineWords.get(firstLineWordIndex - 1);
-        firstLineWords.set(firstLineWordIndex - 1, secondLineWords.get(secondLineWordIndex - 1));
-        secondLineWords.set(secondLineWordIndex - 1, temp);
+        String temp = Array.get(firstLineWords, firstLineWordIndex - 1).toString();
+        Array.set(firstLineWords, firstLineWordIndex - 1, Array.get(secondLineWords, secondLineWordIndex - 1));
+        Array.set(secondLineWords, secondLineWordIndex - 1, temp);
 
-        lines.set(firstLine, String.join(" ", firstLineWords));
-        lines.set(secondLine, String.join(" ", secondLineWords));
+        lines.set(firstLine - 1 , String.join(" ", firstLineWords));
+        lines.set(secondLine - 1, String.join(" ", secondLineWords));
 
 
-        // Write the modified content back to the file
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(wordSwitchRequest.getFilePath()))) {
             for (String updatedLine : lines) {
                 writer.write(updatedLine);
-                writer.newLine(); // Add a newline after each line
+                writer.newLine();
             }
         } catch (IOException e) {
             throw new RuntimeException(e.getMessage());
