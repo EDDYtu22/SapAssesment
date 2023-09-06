33a848c1fcda0ef622ed94b45756696714db9e2f
Edmond Yordanov <edmond.yordanov@mentormate.university.com>
2023-09-06 04:39:28 +0300

    feat: added basic exception handling

--- src/main/java/dev/edmond/sapassesment/exception/DefaultExceptionHandler.java
+++ src/main/java/dev/edmond/sapassesment/exception/DefaultExceptionHandler.java
@@ -1,2 +1,26 @@
-package dev.edmond.sapassesment.exception;public class DefaultExceptionHandler {
+package dev.edmond.sapassesment.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+@ControllerAdvice
+public class DefaultExceptionHandler extends ResponseEntityExceptionHandler{
+
+
+
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleException(IllegalArgumentException illegalArgumentException){
+        return ResponseEntity.badRequest().body(illegalArgumentException.getMessage());
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<String> handleException(RuntimeException runtimeException){
+        return ResponseEntity.badRequest().body(runtimeException.getMessage());
+    }
 }
